e425f54f52001165bc77ced59e58c764fd747c8b
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-04-21 20:11:39 -0300

    Agregué el plugin de vibración. Agregué el plugin de firebase. Agregué el plugin angularfire2. Agregué un botón que hacer vibrar el telefono.

--- trivia/src/pages/home/home.ts
+++ trivia/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Vibration } from '@ionic-native/vibration';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private vibrating : boolean = false;
 
+  constructor(public navCtrl: NavController, private vibration: Vibration) {
+      this.vibration = vibration;
+  }
+
+  toggleVibrator(): void {
+      this.vibrating = true;
+      this.vibration.vibrate(1000);
+      this.vibration.vibrate(0);
+      this.vibrating = false;
   }
 
 }
