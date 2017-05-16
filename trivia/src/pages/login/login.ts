23253ce4deb31cc54649e276dd23f2aae5f0990d
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-05-16 14:58:31 -0300

    Eliminé las biliotecas en login.ts que estaban de mas.

--- trivia/src/pages/login/login.ts
+++ trivia/src/pages/login/login.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { Vibration } from '@ionic-native/vibration';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
-import { Validators, FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
-
 import { JuegoPage } from '../juego/juego';
 
 @Component({
